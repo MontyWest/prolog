f90d5c285f207034ad43ee4bdff317a0fa7e5cab
MontyWest <monty.p.west@gmail.com>
2015-01-20 23:57:05 +0000

    more tests

--- maze-solver-test.pl
+++ maze-solver-test.pl
@@ -1,9 +1,17 @@
 :- ensure_loaded(['maze-solver.pl']).
 
+:- mazeSolver:available_move([1,7], [1,6]).
+:- mazeSolver:available_move([1,7], [2,7]).
+:- \+ mazeSolver:available_move([1,7], [1,8]).
+:- \+ mazeSolver:available_move([1,7], [0,7]).
+:- \+ mazeSolver:available_move([1,7], [2,8]).
+:- \+ mazeSolver:available_move([1,7], [1,9]).
+
 :- mazeSolver:outside(0, 3).
 :- mazeSolver:outside(8, -2).
 :- mazeSolver:outside(10000, 3).
 :- mazeSolver:outside(1, 10000).
+:- \+ mazeSolver:outside(2,2).
 
 :- mazeSolver:adj_tile([2,1], [2,2]).
 :- mazeSolver:adj_tile([3,2], [3,1]).
@@ -12,3 +20,4 @@
 :- \+ mazeSolver:adj_tile([2,1], [1,2]).
 
 :- mazeSolver:endpoints_valid([2,3], [3,4], [[2,3],[3,3],[3,4]]).
+:- \+ mazeSolver:endpoints_valid([2,3], [3,4], [[5,6],[6,6],[6,7]]).
