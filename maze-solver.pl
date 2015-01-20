11951fd825f52be9874d021ca1f73623a4301079
MontyWest <monty.p.west@gmail.com>
2015-01-20 19:48:03 +0000

    equals to is fix

--- maze-solver.pl
+++ maze-solver.pl
@@ -1,5 +1,5 @@
 
-adj_tile([X0,Y0], [X0,Y1]) :- =(Y1, Y0+1).
-adj_tile([X0,Y0], [X0,Y1]) :- =(Y1, Y0-1).
-adj_tile([X0,Y0], [X1,Y0]) :- =(X1, X0+1).
-adj_tile([X0,Y0], [X1,Y0]) :- =(X1, X0-1).
+adj_tile([X0,Y0], [X0,Y1]) :- (Y1 is Y0+1).
+adj_tile([X0,Y0], [X0,Y1]) :- (Y1 is Y0-1).
+adj_tile([X0,Y0], [X1,Y0]) :- (X1 is X0+1).
+adj_tile([X0,Y0], [X1,Y0]) :- (X1 is X0-1).
