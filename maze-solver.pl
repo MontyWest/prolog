36b6ef784ac7f62aded215369122f567c7b43aa8
MontyWest <monty.p.west@gmail.com>
2015-01-20 19:49:58 +0000

    added outside maze rule

--- maze-solver.pl
+++ maze-solver.pl
@@ -1,4 +1,6 @@
 
+outside(X1,Y1) :- mazeSize(A,B), (X1<1), (X1>A), (Y1<1), (Y1>B).
+
 adj_tile([X0,Y0], [X0,Y1]) :- (Y1 is Y0+1).
 adj_tile([X0,Y0], [X0,Y1]) :- (Y1 is Y0-1).
 adj_tile([X0,Y0], [X1,Y0]) :- (X1 is X0+1).
