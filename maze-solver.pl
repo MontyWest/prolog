5177b6b9a31c7312400e091cdf4c40be4fc6947e
MontyWest <monty.p.west@gmail.com>
2015-01-20 20:24:58 +0000

    reverting to old adj_tile for now

--- maze-solver.pl
+++ maze-solver.pl
@@ -3,8 +3,7 @@ available_move([X0,Y0], [X1,Y1]) :- adj_tile([X0,Y0], [X1,Y1]), \+ barrier(X1,Y1
 
 outside(X1,Y1) :- mazeSize(A,B), (X1<1), (X1>A), (Y1<1), (Y1>B).
 
-%% adj_tile([X0,Y0], [X0,Y1]) :- (Y1 is Y0+1); (Y1 is Y0-1).
-%% adj_tile([X0,Y0], [X1,Y0]) :- (X1 is X0+1); (X1 is X0-1).
-adj_tile([X0,Y0], [X1,Y1]) :- (((Y1 is Y0+1); (Y1 is Y0-1)), =(X0,X1)) ;
-							  (((X1 is X0+1); (X1 is X0-1)), =(Y0,Y1)).
+adj_tile([X0,Y0], [X0,Y1]) :- (Y1 is Y0+1); (Y1 is Y0-1).
+adj_tile([X0,Y0], [X1,Y0]) :- (X1 is X0+1); (X1 is X0-1).
+
 
