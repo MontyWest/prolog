993dccc30af57f3d2ce16759d6c2938208cdbd67
MontyWest <monty.p.west@gmail.com>
2015-01-20 19:16:10 +0000

    adding sample maze file

---
+++ maze.pl
@@ -0,0 +1,14 @@
+mazeSize(5, 9).
+barrier(1, 8).
+barrier(2, 1).
+barrier(2, 2).
+barrier(2, 4).
+barrier(2, 5).
+barrier(3, 4).
+barrier(3, 7).
+barrier(3, 9).
+barrier(4, 4).
+barrier(4, 7).
+barrier(4, 8).
+barrier(4, 9).
+barrier(5, 2).
