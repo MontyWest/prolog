d4df6657278375ba1d1411dfd9851d9cc0104452
MontyWest <monty.p.west@gmail.com>
2015-01-26 18:28:06 +0000

    added current tile and end tile to recursive solver, tests failing

--- maze-solver.pl
+++ maze-solver.pl
@@ -23,37 +23,29 @@ adj_tile([X0,Y0], [X0,Y1]) :-
 adj_tile([X0,Y0], [X1,Y0]) :- 
 	(X1 is X0+1); (X1 is X0-1).
 
-solve(A, B, Path) :-
-	startpoint_valid(A, Path),
-	path_valid(B, [], Path).
+solve(ST, ET, Path) :-
+	start_tile_valid(ST, Path),
+	path_valid(ST, ET, [], Path).
 	%% \+ exists_shorter_path(A, B, Path).
 
-exists_shorter_path(A, B, Path) :-
-	endpoints_valid(A, B, OPath),
-	path_valid([], OPath),
-	length(OPath, N),
-	length(Path, M),
-	(N < M),
-	!.
+start_tile_valid(ST, Path) :-
+	Path = [ST|_].
 
-startpoint(SP, Path) :-
-	Path = [SP|_].
+path_valid(CurrentT, ET, Cumu, Path) :-
+	CurrentT = ET.
 
-endpoint_valid(EP, Path) :-
-	last_element(Path, EP).
-
-path_valid(B, _, [[X,Y]]) :-
-	available_tile(X, Y),
-	B = [X,Y].
-	
-path_valid(B, Cumu, Path) :-
-	Path = [C|T],
-	T = [D|_],
-	available_move(C, D),
+path_valid(CurrentT, ET, Cumu, Path) :-
+	available_move(CurrentT, D),
 	\+ is_in(D, Cumu),
-	path_valid([D|Cumu], T).
+	path_valid(D, ET, [D|Cumu], Path).
 
-
+%% exists_shorter_path(A, B, Path) :-
+%% 	endpoints_valid(A, B, OPath),
+%% 	path_valid([], OPath),
+%% 	length(OPath, N),
+%% 	length(Path, M),
+%% 	(N < M),
+%% 	!.
 
 
 
