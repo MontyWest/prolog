0fa48bcf394d01a14d66da72d01a8f067cb8ce1d
MontyWest <monty.p.west@gmail.com>
2015-01-29 16:35:08 +0000

    incresing max depth

--- load.pl
+++ load.pl
@@ -1,6 +1,6 @@
 %% :- working_directory(_, 'Users/montywest/BBK/PPL/Prolog/Coursework').
 
-:- set_prolog_flag(toplevel_print_options, [quoted(true), portray(true), max_depth(30), spacing(next_argument)]).
+:- set_prolog_flag(toplevel_print_options, [quoted(true), portray(true), max_depth(50), spacing(next_argument)]).
 
 :- abolish(barrier/2).
 :- abolish(mazeSize/2).
