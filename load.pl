1bc1e6c7219ef27086e1bace73da16af0bbf36d0
MontyWest <monty.p.west@gmail.com>
2015-01-29 14:54:31 +0000

    added load file

---
+++ load.pl
@@ -0,0 +1,12 @@
+%% :- working_directory(_, 'Users/montywest/BBK/PPL/Prolog/Coursework').
+
+:- set_prolog_flag(toplevel_print_options, [quoted(true), portray(true), max_depth(30), spacing(next_argument)]).
+
+:- abolish(barrier/2).
+:- abolish(mazeSize/2).
+
+:- use_module('maze-big.pl', [mazeSize/2, barrier/2]).
+%% :- use_module('maze.pl', [mazeSize/2, barrier/2]).
+:- use_module('grid-printer.pl', [printGrid/1]).
+:- use_module('maze-solver.pl', [solve/3]).
+
